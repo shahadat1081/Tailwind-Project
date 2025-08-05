87f7fc4b895cba92e20c1ec243cdb72257c3dd99
shahadat1081 <sheikshahadat4@gmail.com>
2025-08-06 00:43:47 +0600

    full project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["*"],
+  theme: {
+    extend: {
+      "fontFamily":{
+        'open':[' "Open Sans", sans-serif']
+      }
+    },
+     
+  },
+  plugins: [],
+}
+
